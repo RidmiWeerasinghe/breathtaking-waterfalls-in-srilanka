3ef52988a672a2dc11d4ce17bdb53e9032549076
DevRidmi <guinevere1998in@gmail.com>
2024-06-12 18:04:52 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,234 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    
+    <link rel="shortcut icon" href="images/icon.png" type="image/x-icon">
+
+    <title>The most breathtaking waterfalls in srilanka</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js"></script>
+	  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet" />
+    
+    <style>
+    
+      
+    </style>
+
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+    <?php
+
+    // // Get the client IP address and user agent string
+    // $ip = $_SERVER['REMOTE_ADDR'];
+    // $user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+    // // Build the log entry string
+    // $log_entry = "[" . date("Y-m-d H:i:s") . "] " . $ip . " " . $user_agent . "\n";
+
+    // // Open the log file for writing
+    // $log_file = fopen('visitor_log.txt', 'a');
+
+    // // Write the log entry to the file
+    // fwrite($log_file, $log_entry);
+
+    // // Close the log file
+    // fclose($log_file);
+
+    // ?>
+
+      <header>
+        <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="/../"><img src="icon.png" width="50" height= auto ></a>
+                <a class="navbar-brand" href="/../"><h3> Waterfalls</h3></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <ul class="navbar-nav ms-auto mb-2 mb-md-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/../">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Waterfalls
+                          </a>
+                          <ul class="dropdown-menu bg-dark">
+                            <li><a class="dropdown-item text-white p-2 text-center" href="StClairs/">St. Clair's Falls</a></li>
+                            <li><a class="dropdown-item text-white p-2 text-center" href="Devon/">Devon Falls</a></li>
+                            <li><a class="dropdown-item text-white p-2 text-center" href="Aberdeen/">Aberdeen Falls</a></li>
+                            <li><a class="dropdown-item text-white p-2 text-center" href="Laxapana/">Laxapana Falls</a></li>
+                            <li><a class="dropdown-item text-white p-2 text-center" href="Bakers/">Baker’s Falls</a></li>
+                            <li><a class="dropdown-item text-white p-2 text-center" href="Bambarakanda/">Bambarakanda Falls</a></li>
+                          </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="About/">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Contact/" tabindex="-1" aria-disabled="false">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                          <a class="nav-link" href="SignIn/">Sign In</a>
+                      </li>
+                      </ul>
+                </div>
+            </div>
+        </nav>
+      </header>
+
+      <main>
+
+          <div id="demo" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
+              <div class="carousel-indicators">
+                  <button type="button" data-bs-target="#demo" data-bs-slide-to="0" class="active"></button>
+                  <button type="button" data-bs-target="#demo" data-bs-slide-to="1"></button>
+                  <button type="button" data-bs-target="#demo" data-bs-slide-to="2"></button>
+              </div>
+
+              <div class="carousel-inner text-center">
+                  <div class="carousel-item active">
+                      <img src="images/15.jpg" alt="Los Angeles" class="d-block" style="width:100%">
+                      <div class="carousel-caption">
+                          <h1 class="text-white display-1" style="font-family: Trebuchet MS;">Breathtaking Waterfalls <br> in Sri Lanka</h1>
+                          <p class="text-white">“There's no time to be bored in a world as beautiful as this”</p>
+                      </div>
+                      
+                  </div>
+                  <div class="carousel-item">
+                      <img src="images/27.jpg" alt="Chicago" class="d-block" style="width:100%">
+                      <div class="carousel-caption">
+                          <h1 class="text-white display-1" style="font-family: Trebuchet MS;">Breathtaking Waterfalls <br> in Sri Lanka</h1>
+                          <p class="text-white">“There's no time to be bored in a world as beautiful as this”</p>
+                      </div>
+                  </div>
+                  <div class="carousel-item">
+                      <img src="images/17.jpg" alt="New York" class="d-block" style="width:100%">
+                      <div class="carousel-caption">
+                          <h1 class="text-white display-1" style="font-family: Trebuchet MS;">Breathtaking Waterfalls <br> in Sri Lanka</h1>
+                          <p>“There's no time to be bored in a world as beautiful as this”</p>
+                      </div>
+                  </div>
+              </div>
+
+             
+              <button class="carousel-control-prev" type="button" data-bs-target="#demo" data-bs-slide="prev">
+                  <span class="carousel-control-prev-icon"></span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#demo" data-bs-slide="next">
+                  <span class="carousel-control-next-icon"></span>
+              </button>
+          </div>
+          
+          <div class="container mt-3">
+              <div class="row featurette">
+                  <div class="col-md-7">
+                      <h1 class="featurette-heading" style="font-family: Trebuchet MS;">Water is the cure to everything</h1>
+                      <br>
+                      <p class="lead">“While people are struggling unhappily in the cities against the cruel authorities, a waterfall happily and cheerfully flows in the nature; there is happiness only if there is freedom!” – Mehmet Murat Ildan</p>
+                  </div>
+                  <div class="col-md-5">
+                      <img src="images/16.jpg" class="rounded img-fluid mx-auto" alt="Cinque Terre" width=800 height=auto>
+                  </div>
+              </div>
+          </div>
+
+          <hr class="featurette-divider">
+          <div class="container mt-3">
+              <div class="row featurette">
+                  <div class="col-md-7 order-md-2">
+                      <h1 class="featurette-heading" style="font-family: Trebuchet MS;"> <span class="text-muted">See for yourself</span></h1><br>
+                      <p class="lead">We’re always looking for the most beautiful waterfalls around the world. Whether it’s the tropical stunners in the Philippines or
+                         the pine forested wild ones in Oregon, we never grow tired of seeing these beautiful natural wonders. It’s hard to not look up at a waterfall 
+                         and feel inspired, the power and the beauty always leave us mesmerised.</p>
+                  </div>
+                  <div class="col-md-5 order-md-1">
+                      <img src="images/24.jpg " class="rounded img-fluid mx-auto" alt="Cinque Terre" width=auto height="400">
+                  </div>
+              </div>
+          </div>
+
+         
+
+          <hr class="featurette-divider">
+
+          </div>
+
+          <div class="container marketing">
+
+              <div class="row">
+                  <div class="col-lg-4">
+                      <a href="StClairs/"><img src="images/o8.jpg" class="rounded-circle" alt="St. Clair's Falls" width="350" height="350"></a>
+
+                      <a href="StClairs/" style="text-decoration:none; color: #5a5a5a;"><h2>St. Clair's Falls</h2></a>
+                      <p><i>Little Niagara</i></p>
+                      </div>
+                  <div class="col-lg-4">
+                    <a href="Devon/"><img src="images/o2.jpg" class="rounded-circle" alt="Devon Falls" width="350" height="350"></a>
+
+                    <a href="Devon/" style="text-decoration:none; color: #5a5a5a;"><h2>Devon Falls</h2></a>
+                      <p><i>Veil of the Valley</i></p>
+                    </div>
+                  <div class="col-lg-4">
+                    <a href="Aberdeen/"><img src="images/o1.jpg" class="rounded-circle" alt="Aberdeen Falls" width="350" height="350"></a>
+
+                    <a href="Aberdeen/" style="text-decoration:none; color: #5a5a5a;"><h2>Aberdeen Falls</h2></a>
+                      <p><i>Alidona Falls</i></p></div>
+              </div>
+
+          </div>
+
+          <div class="container marketing">
+
+              <div class="row">
+                  <div class="col-lg-4">
+                    <a href="Laxapana/"><img src="images/o4.jpg" class="rounded-circle" alt="Laxapana Falls" width="350" height="350"></a>
+
+                    <a href="Laxapana/" style="text-decoration:none; color: #5a5a5a;"><h2>Laxapana Falls</h2></a>
+                      <p><i>Laksha-pahana</i></p>
+                    </div>
+                  <div class="col-lg-4">
+                    <a href="Bakers/"><img src="images/o5.jpg" class="rounded-circle" alt="Baker’s Falls" width="350" height="350"></a>
+
+                    <a href="Bakers/" style="text-decoration:none; color: #5a5a5a;"><h2>Baker’s Falls</h2></a>
+                      <p><i>Gonagala Ella</i></p>
+                    </div>
+                  <div class="col-lg-4">
+                    <a href="Bambarakanda/"><img src="images/o6.jpg" class="rounded-circle" alt="Bambarakanda Falls" width="350" height="350"></a>
+
+                    <a href="Bambarakanda/" style="text-decoration:none; color: #5a5a5a;"><h2>Bambarakanda Falls</h2></a>
+                      <p><i>Bambarakele Falls</i></p>
+                    </div>
+              </div>
+        </main>
+     </body>
+     <footer class="text-center text-lg-start bg-dark mt-3">
+      <div class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-dark">
+          
+        <div class="text-white mb-3 mb-md-0">
+          Copyright © 2020. All rights reserved.
+        </div>
+  
+        <div>
+          <a href="https://www.facebook.com/" target="_blank" class="text-white me-4" style="text-decoration:none;">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+          <a href="https://twitter.com/i/flow/login" target="_blank" class="text-white me-4" style="text-decoration:none;">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="https://accounts.google.com/v3/signin/identifier?dsh=S-1426045677%3A1668236958348465&continue=https%3A%2F%2Faccounts.google.com%2F&followup=https%3A%2F%2Faccounts.google.com%2F&passive=1209600&flowName=GlifWebSignIn&flowEntry=ServiceLogin&ifkv=ARgdvAvVi9UO1LvnMxRy2nNekDExVO45iuyVds14okEF9DxCAmpIcxgvgId000KSubO0XWMvbIGFxw" target="_blank" class="text-white me-4" style="text-decoration:none;">
+            <i class="fab fa-google"></i>
+          </a>
+          <a href="https://www.linkedin.com/login" target="_blank" class="text-white" style="text-decoration:none;">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+</html>
